9bd5675397a911ddf7276b8d205caf07f60502cf
Nahuelnp <nahuelyad@gmail.com>
2020-12-28 22:45:40 -0300

    terminado

---
+++ myblog/src/main/java/com/informatorio/myblog/src/main/java/com/informatorio/myblog/model/User.java
@@ -0,0 +1,132 @@
+package com.informatorio.myblog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.TimeZone;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import javax.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    @Size(min = 3)
+    private String name;
+
+    @Column(nullable = false)
+    @Size(min = 3)
+    private String surname;
+
+    @Column(nullable = false, unique = true)
+    @Size(min = 3)
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 8)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Column(nullable = false)
+    @Size(min = 3)
+    private String city;
+
+    @Column(nullable = false)
+    @Size(min = 3)
+    private String province;
+
+    @Column(nullable = false)
+    @Size(min = 3)
+    private String country;
+
+    
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate registDate = LocalDate.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LocalDate getRegistDate() {
+        return registDate;
+    }
+
+    public void setRegistDate(LocalDate registDate) {
+        this.registDate = registDate;
+    }
+
+
+
+}
